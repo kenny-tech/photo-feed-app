e2a72dd3b2400e749c35e429fb0d2f6853ee5cf1
kenny-tech <kennyemma2008@gmail.com>
2020-02-28 15:58:27 +0100

    Created navigation

---
+++ app/screens/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Profile = () => (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <Text>Profile screen</Text>
+    </View>
+);
+
+export default Profile;
---
+++ app/screens/Upload.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Upload = () => (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+        <Text>Upload screen</Text>
+    </View>
+);
+
+export default Upload;
