853ab73c49fe3f473e77dae0a1090ae4f25a61f9
kenny-tech <kennyemma2008@gmail.com>
2020-04-06 23:15:17 +0100

    Updated Upload screen

--- app/screens/Upload.js
+++ app/screens/Upload.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
 const options = {
@@ -49,10 +49,19 @@ class Upload extends React.Component {
           
             if (response.didCancel) {
               console.log('User cancelled image picker');
+              this.setState({
+                imageSelected: false,
+              });
             } else if (response.error) {
               console.log('ImagePicker Error: ', response.error);
+              this.setState({
+                imageSelected: false,
+              });
             } else if (response.customButton) {
               console.log('User tapped custom button: ', response.customButton);
+              this.setState({
+                imageSelected: false,
+              });
             } else {
               const source = { uri: response.uri };
               // You can also display the image using data:
@@ -60,31 +69,54 @@ class Upload extends React.Component {
           
               this.setState({
                 avatarSource: source,
+                imageSelected: true,
+                imageId: this.uniqueId(),
+                uri: response.uri
               });
-            //   console.log('Uploaded image: ',this.state.avatarSource);
+              let image = this.state.avatarSource;
+
+              // console.log('Uploaded image: ',this.state.avatarSource);
               this.uploadImage(this.state.avatarSource);
             }
           });
     }
 
     uploadImage = (image) => {
+        // upload image to server
         console.log('Uploaded Image: ',image)
     }
 
     render() {
+        const { loggedin, imageSelected } = this.state;
+        
         return (
             <View style={styles.container}>
                 {
-                    this.state.loggedin == true ? 
+                    loggedin ? 
                     (
                     <View style={styles.container}>
-                        <Text style={{fontSize: 28, paddingBottom: 15}}>Upload</Text>
+                        { imageSelected ? (<View style={{flex: 1}}><View>
+                            <Text style={{fontSize: 28, paddingBottom: 15}}>Upload</Text>
+                        </View>
+                        <View>
+                            <Text style={{margin: 5}}>Caption: </Text>
+                            <TextInput
+                                editable={true}
+                                placeholder={'Enter your caption...'}
+                                maxLength={150}
+                                multiline={true}
+                                numberOfLine={4}
+                                onChangeText={(text) => this.setState({caption: text})}
+                                style={{marginVertical:10, height:100, width: 300, padding:5, boderColor: 'grey', borderWidth: 1, borderRadius:3, backgroundColor:'white', color:'black'}}
+                            />
+                            </View>
+                        </View>) : (<View style={{flex:1, justifyContent:'center', alignItems:'center'}}><Text style={{fontSize: 28, paddingBottom: 15}}>Upload</Text>
                         <TouchableOpacity
                         style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'blue', borderRadius: 5}}
                         onPress={() => this.findNewImage()}>
                             <Text style={{color: 'white'}}>Select Photo</Text>
                         </TouchableOpacity>
-                        <Image source={this.state.avatarSource} style={{width: 500, height: 300}}/>
+                        <Image source={this.state.avatarSource} style={{width: 500, height: 300}}/></View>)}
                     </View>): (
                     <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
                         <Text>You are not logged in </Text>
@@ -100,8 +132,8 @@ class Upload extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
+        justifyContent:'center', 
+        alignItems:'center'
     }
 })
 
